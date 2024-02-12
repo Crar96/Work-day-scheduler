0411e7440847c802aa46d28930026788f7052ab2
C Grogan <conorgrogan23@gmail.com>
2024-02-12 19:25:39 +0000

    added function to display current date

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // Function to display current day and timeblocks
+
+$(document).ready(function() {
+  // Display current day at the top of the calendar
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+});
