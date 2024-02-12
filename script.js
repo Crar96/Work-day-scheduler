7430240c7c46fbedb8cf2c084e92d79c88cd6585
C Grogan <conorgrogan23@gmail.com>
2024-02-12 20:32:45 +0000

    added conditional statement for past present and future

--- script.js
+++ script.js
@@ -7,8 +7,20 @@ $(document).ready(function() {
 for (let i = 9.00; i <= 17.00; i++) {
   let timeBlock = $("<div>").addClass("row time-block");
   let column = $("<div>").addClass("col-md-1 hour").text(dayjs().hour(i).format("hA"));
-  let textBox = $("<textbox").addClass("col-md-10 description");
+  let textArea = $("<textArea").addClass("col-md-10 description");
 }
+
+// Check if time is past, present or future and delegate classes for styling
+
+if (dayjs().hour() === i) {
+  textArea.addClass("present");
+} else if (dayjs().hour() > i) { 
+  textArea.addClass("past");
+} else {
+  textArea.addClass("future");
+}
+
+
 
 });
 
