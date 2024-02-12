1c5100c7c9d9cbe3b600223743b8d6880885d40a
C Grogan <conorgrogan23@gmail.com>
2024-02-12 21:48:53 +0000

    fixed syntax error in textArea for loop - missing > was causing errors

--- script.js
+++ script.js
@@ -4,37 +4,38 @@ $(document).ready(function() {
 
 
 // Time blocks 
-for (let i = 9.00; i <= 17.00; i++) {
-  let timeBlock = $("<div>").addClass("row time-block");
-  let column = $("<div>").addClass("col-md-1 hour").text(dayjs().hour(i).format("hA"));
-  let textArea = $("<textArea").addClass("col-md-10 description");
-}
+  for (let i = 9.00; i <= 17.00; i++) {
+    let timeBlock = $("<div>").addClass("row time-block");
+    let column = $("<div>").addClass("col-md-1 hour").text(dayjs().hour(i).format("hA"));
+    let textArea = $("<textArea>").addClass("col-md-10 description");
 
 // Check if time is past, present or future and delegate classes for styling
 
-if (dayjs().hour() === i) {
-  textArea.addClass("present");
-} else if (dayjs().hour() > i) { 
-  textArea.addClass("past");
-} else {
-  textArea.addClass("future");
-}
+  if (dayjs().hour() === i) {
+    textArea.addClass("present");
+  } else if (dayjs().hour() > i) { 
+    textArea.addClass("past");
+  } else {
+    textArea.addClass("future");
+  }
 
-let storedEvent = localStorage.getItem("event" + i);
-if (storedEvent) {
-  textArea.val(storedEvent);
-}
+  let storedEvent = localStorage.getItem("event" + i);
+  if (storedEvent) {
+    textArea.val(storedEvent);
+  }
 
-let saveBtn = $("<button>").addClass("col-md-1 saveBtn").html('<i class="fas fa-save"></i>');
+  let saveBtn = $("<button>").addClass("col-md-1 saveBtn").html('<i class="fas fa-save"></i>');
 
-});
+  saveBtn.on("click", function() {
+    let eventText = textArea.val();
+    localStorage.setItem("event" + i, eventText);
+  });
 
 // Append timeblock and elements
 
-timeBlock.append(column, textBox,);
-
-$(".container").append(timeBlock);
-
-
+  timeBlock.append(column, textArea, saveBtn);
+  $(".container").append(timeBlock);
+  }
+});
 
 
